ba6006f41058ff797a3a67ed0cd10a05caf8adba
Junth Basnet <junthbasnet@gmail.com>
2019-11-14 16:20:22 +0545

    Recommendation using SVD completed.

--- Singular Value Decomposition (SVD)/SVD.py
+++ Singular Value Decomposition (SVD)/SVD.py
@@ -1,6 +1,21 @@
 from MovieLens import MovieLens
 from surprise import SVD
 
+
+def buildAntiTestSetForUser(testSubject, trainSet):
+    fill = trainSet.global_mean
+
+    anti_testset = []
+
+    u = trainSet.to_inner_uid(str(testSubject))
+    user_items = set([j for (j, _) in trainSet.ur[u]])
+
+    anti_testset += [(trainSet.to_raw_uid(u), trainSet.to_raw_iid(i), fill) for
+                     i in trainSet.all_items() if
+                     i not in user_items]
+    return anti_testset
+
+
 # Picking arbitrary test subject
 testSubject = 85
 ml = MovieLens()
@@ -19,11 +34,11 @@ for ratings in userRatings:
     if (float(ratings[1]) < 3.0):
         hatedMovies.append(ratings)
 
-print('\nUser {} loved these movies...'.format(testSubject))
+print('\nUser {} loved these movies:'.format(testSubject))
 for ratings in lovedMovies:
     print(ml.getMovieName(ratings[0]))
 
-print('\nUser {} hated these movies...'.format(testSubject))
+print('\nUser {} hated these movies:'.format(testSubject))
 for ratings in hatedMovies:
     print(ml.getMovieName(ratings[0]))
 
@@ -32,3 +47,17 @@ trainSet = data.build_full_trainset()
 
 algo = SVD()
 algo.fit(trainSet)
+
+print('Computing recommendations...')
+testSet = buildAntiTestSetForUser(testSubject, trainSet)
+predictions = algo.test(testSet)
+
+recommendations = []
+
+print('\nMovie Recommendation System recommends:')
+for userID, movieID, actualRating, estimatedRating, _ in predictions:
+    intMovieID = int(movieID)
+    recommendations.append((intMovieID, estimatedRating))
+
+for ratings in recommendations[:10]:
+    print(ml.getMovieName(ratings[0]))
