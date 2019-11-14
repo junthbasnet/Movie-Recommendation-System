bc42c231429f622265cb09d2dd1de204d84b40f4
Junth Basnet <junthbasnet@gmail.com>
2019-11-14 17:51:59 +0545

    Initial Commit: TestMetrics

---
+++ EvaluatingPerformance/TestMetrics.py
@@ -0,0 +1,24 @@
+from MovieLens import MovieLens
+from RecommenderMetrics import RecommenderMetrics
+from surprise import SVD
+from surprise import KNNBaseline
+from surprise.model_selection import train_test_split
+from surprise.model_selection import LeaveOneOut
+
+ml = MovieLens()
+
+print('Loading movie ratings...')
+data = ml.loadMovieLensLatestSmall()
+
+print('\nBuilding Recommendation model...')
+trainSet, testSet = train_test_split(data, test_size = .25, random_state = 1)
+
+algo = SVD(random_state = 10)
+algo.fit(trainSet)
+
+print('\nComputing Recommendations...')
+predictions = algo.test(testSet)
+
+print('\nEvaluating accuracy of the recommendation model...')
+print('RMSE (Root Mean Squared Error): {}'.format(RecommenderMetrics.RMSE(predictions)))
+print('MAE (Mean Absolute Error): {}'.format(RecommenderMetrics.MAE(predictions)))
