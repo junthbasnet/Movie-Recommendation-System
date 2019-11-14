d82e57e938904de2b38a7918c8333b5593b4b768
Junth Basnet <junthbasnet@gmail.com>
2019-11-14 08:38:01 +0545

    SVD Initial Commit

---
+++ Singular Value Decomposition (SVD)/SVD.py
@@ -0,0 +1,28 @@
+from MovieLens import MovieLens
+from surprise import SVD
+
+# Picking arbitrary test subject
+testSubject = 85
+ml = MovieLens()
+
+print("Loading movie ratings...")
+data = ml.loadMovieLensLatestSmall()
+
+userRatings = ml.getUserRatings(testSubject)
+
+lovedMovies = []
+hatedMovies = []
+
+for ratings in userRatings:
+    if (float(ratings[1]) > 4.0):
+        lovedMovies.append(ratings)
+    if (float(ratings[1]) < 3.0):
+        hatedMovies.append(ratings)
+
+print('\nUser {} loved these movies...'.format(testSubject))
+for ratings in lovedMovies:
+    print(ml.getMovieName(ratings[0]))
+
+print('\nUser {} hated these movies...'.format(testSubject))
+for ratings in hatedMovies:
+    print(ml.getMovieName(ratings[0]))
